df685e9d566f63bf17db5166ce0189e30ce9b225
Brinta Midya <brintamidya@gmail.com>
2021-02-19 10:50:55 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 	
 	@RequestMapping("/")
 	public String index() {
-		return "Hello Spring Pipeline again !!!";
+		return "Hello Spring Pipeline again again !!!";
 	}
 	
 }
